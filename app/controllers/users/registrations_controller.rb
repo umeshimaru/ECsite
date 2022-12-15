70b4ce7165c99094a0e42ee760d35826a70bc02b
masa <amtgptwmjgad@yahoo.co.jp>
2022-12-15 11:05:22 +0000

    05 create login system

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,7 +11,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   def create
+    
     super
+    
   end
 
   # GET /resource/edit
@@ -44,15 +46,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   protected
 
-  # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  end
+    # If you have extra params to permit, append them to the sanitizer.
+def configure_sign_up_params
+     devise_parameter_sanitizer.permit(
+       :sign_up, keys: [ :name, :postal_code, :address, :phone, :email, :password, :password_confirmation ])
+end
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  end
+def configure_account_update_params
+     devise_parameter_sanitizer.permit(
+       :account_update, keys: [ :name, :postal_code, :address, :phone, :email, :password, :password_confirmation ])
+end
+
+ 
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
